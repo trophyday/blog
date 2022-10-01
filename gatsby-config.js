8f8854174754da32eef63a147b76be2522a88d05
Sundar Sritharan <sundar.sritharan@gmail.com>
2022-10-01 15:09:20 -0400

    small set of changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Trophy Protocol`,
+    siteTitleAlt: `Trophy Protocol - Web3 Reputation Protocol`,
+    siteHeadline: `Trophy Protocol - Web3 Reputation Protocol | Soulbound Tokens(SBTs)`,
+    siteUrl: `https://trophyday.xyz`,
+    siteDescription: ` Trophy is a web3 reputation protocol for anyone to manage their Soulbound tokens (SBTs).`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@sundarsritharan`,
   },
   plugins: [
     {
